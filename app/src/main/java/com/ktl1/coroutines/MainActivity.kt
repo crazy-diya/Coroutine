75a38914133b5685dac261458dbe01f03a932b6b
dimuthu_l <dimuthu_l@epiclanka.net>
2022-05-29 11:17:40 +0530

    this is how switch coroutine between thread one to another

--- app/src/main/java/com/ktl1/coroutines/MainActivity.kt
+++ app/src/main/java/com/ktl1/coroutines/MainActivity.kt
@@ -5,29 +5,28 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
 
     private var count = 0
+    private lateinit var textView2: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val textView = findViewById<TextView>(R.id.tvCount)
+        textView2 = findViewById<TextView>(R.id.tvText1)
         val countButton = findViewById<Button>(R.id.btnCount)
         val downloadButton = findViewById<Button>(R.id.downloadBtn)
 
-        countButton.setOnClickListener{
+        countButton.setOnClickListener {
             textView.text = count++.toString()
         }
 
-        downloadButton.setOnClickListener{
+        downloadButton.setOnClickListener {
 
             // use coroutineScope for Sycronesly work
             CoroutineScope(Dispatchers.IO).launch {
@@ -38,8 +37,11 @@ class MainActivity : AppCompatActivity() {
     }
 
     private suspend fun downloadUserData() {
-        for(i in 1..2000000){  // 3..9
-            Log.i("myTag","Download user $i ${Thread.currentThread().name}")
+        for (i in 1..2000000) {  // 3..9
+            Log.i("myTag", "Download user $i ${Thread.currentThread().name}")
+            withContext(Dispatchers.Main){  // this is how switch coroutine between thread one to another
+                textView2.text = i.toString()
+            }
             delay(100) // use for late running log after the one by one
         }
     }
